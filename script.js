36d44c19bc7936046da288939536ccfcdacd8a0c
Miguel Bueno <47156304+bueno766@users.noreply.github.com>
2022-10-11 00:54:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+let cont = 0;
+let input = document.getElementById('campo');
+let btnAdd = document.getElementById('btnAdd');
+let main = document.getElementById('areaLista');
+btnAdd.addEventListener('click', add);
+
+function add(){
+    //pegar valor digitado
+    let valor = input.value;
+
+    //se não tiver campo vazio, nulo ou indefinido
+    if((valor !=="") && (valor !==null) && (valor !==undefined)){
+        ++cont;
+        let novoItem = ` <div id="${cont}" class="item">
+            <div onclick="marcar(${cont})" class="item-icone">
+                <i id="icone_${cont}" class="mdi mdi-circle-outline"></i>
+            </div>
+            <div onclick="marcar(${cont})" class="item-nome">
+                ${valor}
+            </div>
+            <div class="item-botao">
+                <button onclick="deletar(${cont})" class="delete"><i class="mdi mdi-delete"></i></i>  Deletar</button>
+            </div>
+        </div>`;
+
+        //Adiconar nova tarefa na area
+        main.innerHTML += novoItem;
+
+        //zerar campo ao adicionar
+        input.value = "";
+        input.focus();
+    }
+
+}
+function deletar(id){
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+function marcar(id){
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+
+    if(classe =="item"){
+        item.classList.add('clicado');
+        var icone = document.getElementById('icone_'+id);
+        icone.classList.remove('mdi-circle-outline');
+        icone.classList.add('mdi-check-circle');
+        //adicionar item selecionado para final
+        item.parentNode.appendChild(item);
+    }else{
+        item.classList.remove('clicado');
+        var icone = document.getElementById('icone_'+id);
+        icone.classList.remove('mdi-check-circle');
+        icone.classList.add('mdi-circle-outline');
+    }
+}
+
+input.addEventListener("keyup", function(event){
+    //Se clicou enter
+    if(event.keyCode === 13){
+        event.preventDefault();
+        btnAdd.click();
+    }
+});
+
+
